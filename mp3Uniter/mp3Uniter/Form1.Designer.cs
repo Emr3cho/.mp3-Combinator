2f51e2209021846e80d02065a5da4a363fe5ffc0
Emre Myumyun <emre.metin2002@gmail.com>
2021-03-01 00:04:48 +0200

    Add files via upload

---
+++ mp3Uniter/mp3Uniter/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace mp3Uniter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_Add = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Btn_Add
+            // 
+            this.Btn_Add.Location = new System.Drawing.Point(12, 12);
+            this.Btn_Add.Name = "Btn_Add";
+            this.Btn_Add.Size = new System.Drawing.Size(729, 145);
+            this.Btn_Add.TabIndex = 0;
+            this.Btn_Add.Text = "button1";
+            this.Btn_Add.UseVisualStyleBackColor = true;
+            this.Btn_Add.Click += new System.EventHandler(this.Btn_Add_Click);
+            this.Btn_Add.DragDrop += new System.Windows.Forms.DragEventHandler(this.Btn_Add_DragDrop);
+            this.Btn_Add.DragEnter += new System.Windows.Forms.DragEventHandler(this.Btn_Add_DragEnter);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 56);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 360);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Btn_Add);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_Add;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ mp3Uniter/mp3Uniter/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace mp3Uniter
+{
+
+    public partial class Form1 : Form
+    {
+        List<byte> allBytes = new List<byte>();
+        Queue<string> fileNames = new Queue<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Add_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            MessageBox.Show(files[0]);
+        }
+
+        private void Btn_Add_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+        public string TakeFile()
+        {
+            OpenFileDialog source = new OpenFileDialog();
+
+            if ((source.ShowDialog()) == DialogResult.OK)
+            {
+                return source.FileName;
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+        static void Unioner(Queue<string> names)
+        {
+            bool activate = false;
+            string fileToRead = names.Dequeue();
+
+            using (FileStream writer = new FileStream(@"C:\Users\DESKTOP PC\Desktop\as\okay.mp3", FileMode.Create))
+            {
+                while (names.Count > 0)
+                {
+                    if (activate)
+                    {
+                        fileToRead = names.Dequeue();
+                    }
+
+                    using (FileStream reader = new FileStream(fileToRead, FileMode.Open))
+                    {
+                        byte[] buffer = new byte[(int)Math.Pow((double)2,20)];
+
+                        while (true)
+                        {
+                            int readedBytes = reader.Read(buffer, 0, buffer.Length);
+
+
+                            if (readedBytes == 0)
+                            {
+                                activate = true;
+                                break;
+                            }
+
+                            if (readedBytes < buffer.Length)
+                            {
+                                buffer = buffer.Take(readedBytes).ToArray();
+                            }
+
+                            writer.Write(buffer, 0, buffer.Length);
+                        }
+
+                    }
+                }
+            }
+        }
+
+        public bool InputValidator(string input)
+        {
+            bool inputValidator = false;
+
+            Regex mp3InputValidator = new Regex(@"^[\w\D]*\.mp3");
+
+            if (mp3InputValidator.IsMatch(input))
+            {
+                inputValidator = true;
+            }
+
+            if (inputValidator)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void Btn_Add_Click(object sender, EventArgs e)
+        {
+            string fileName = TakeFile();
+            if (InputValidator(fileName))
+            {
+                Btn_Add.Text = fileName;
+                fileNames.Enqueue(fileName);
+            }
+            else
+            {
+                MessageBox.Show("Невалидно съдържание! Програмата обединява само файлове с .mp3 удължения!"
+                    , "ГРЕШКА!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Unioner(fileNames);
+        }
+    }
+}
